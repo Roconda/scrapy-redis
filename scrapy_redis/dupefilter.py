f4c0a2b92aa8d5047d25fe59168aad483d792d0f
Tim Slot <tim.slot@gmail.com>
2015-07-06 21:54:40 +0200

    Add expire time to dupe keys

--- scrapy_redis/dupefilter.py
+++ scrapy_redis/dupefilter.py
@@ -8,6 +8,7 @@ from . import connection
 
 class RFPDupeFilter(BaseDupeFilter):
     """Redis-based request duplication filter"""
+    ttl = -1
 
     def __init__(self, server, key):
         """Initialize duplication filter
@@ -17,6 +18,7 @@ class RFPDupeFilter(BaseDupeFilter):
         server : Redis instance
         key : str
             Where to store fingerprints
+        ttl: Dupe expire time in seconds
         """
         self.server = server
         self.key = key
@@ -28,6 +30,11 @@ class RFPDupeFilter(BaseDupeFilter):
         # class as standalone dupefilter with scrapy's default scheduler
         # if scrapy passes spider on open() method this wouldn't be needed
         key = "dupefilter:%s" % int(time.time())
+
+        # Check if TTL settings is available.
+        if hasattr(settings, "DUPE_TTL"):
+            cls.ttl = settings.DUPE_TTL
+
         return cls(server, key)
 
     @classmethod
@@ -37,6 +44,10 @@ class RFPDupeFilter(BaseDupeFilter):
     def request_seen(self, request):
         fp = request_fingerprint(request)
         added = self.server.sadd(self.key, fp)
+
+        if self.ttl > 0:
+            self.server.expire(self.key, self.ttl)
+
         return not added
 
     def close(self, reason):
